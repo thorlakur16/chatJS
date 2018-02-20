7f64227bc8fe3fe846ff53ff192b301b3e99aec2
audurr16 <audurr16@ru.is>
2018-02-20 22:10:00 +0000

    some tests

--- src/components/MakeRoom/MakeRoom.spec.js
+++ src/components/MakeRoom/MakeRoom.spec.js
@@ -1,6 +1,15 @@
 //define tests for Login
+import React from 'react';
+import {shallow} from 'enzyme';
+import MakeRoom from './MakeRoom';
+
 describe('MakeRoom tests', () => {
-    it('should pass', () => {
-        expect(true).toBe(true);
+    let component;
+    beforeEach(() => {
+        component = shallow(<MakeRoom/>);
+    });
+
+    it('should contain a label Make new room:', () => {
+        expect(component.contains(<label>Make new room:</label>));
     });
 });
