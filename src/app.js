30702bf75160739207433ad71c8848efb4962915
unknown <binnib5@gmail.com>
2018-02-20 14:09:51 +0000

    testið þetta breytti chatJS í part1 gæti verið path vesen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import MainContent from './components/MainContent/MainContent';
 //import Login from './components/Login/Login';
 //import UserList from './components/UserList/UserList';
 import '../styles/site';
+import {Switch, Route,BrowserRouter, Redirect,withRouter} from 'react-router-dom';
+import Login from './components/Login/Login';
 //import RoomList from './components/RoomList/RoomList';
 //import ChatWindow from './components/ChatWindow/ChatWindow';
 //import MakeRoom from './components/MakeRoom/MakeRoom';
@@ -20,10 +22,47 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            user:'',
+            userLogedIn:false
+        };
+        this.userHandler = this.userHandler.bind(this);
+        this.loginHandler = this.loginHandler.bind(this);
+    }
+
+    userHandler(nickname) {
+        this.setState({users:nickname});
+        console.log(nickname);
+
+    }
+
+    loginHandler(signedIn) {
+        this.setState({userLogedIn:signedIn});
+        console.log(this.state.userLogedIn);
+        if(this.state.userLogedIn) {
+            this.props.history.push('/chatboard');
+            console.log('push worked');
+        }
     }
     render() {
+        const {isLogedIn} = this.state.userLogedIn;
         return (
-            <MainContent />
+            <Switch>
+                <Route exact path='/' render={ () => {
+                    if(isLogedIn) {
+                        return <Redirect to='/chatboard'/>;
+                    }else {
+                        return <Redirect to='/login'/>;
+                    }
+                }
+                }/>
+                <Route path='/login' render={(routeProps) => (
+                    <Login {...routeProps} userHandler={this.userHandler} loginHandler={this.loginHandler}/>
+                )}/>
+                <Route path='/chatboard' component={MainContent} />
+            </Switch>
+
         );
     }
 }
@@ -32,5 +71,6 @@ App.childContextTypes = {
     socket: PropTypes.object.isRequired
 };
 
+const AppWithRouter = withRouter(App);
+ReactDOM.render(<BrowserRouter><AppWithRouter /></BrowserRouter>, document.getElementById('app'));
 
-ReactDOM.render(<App />, document.getElementById('app'));
